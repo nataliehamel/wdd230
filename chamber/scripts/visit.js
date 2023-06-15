a9401e190130e656ab7593382b24f0b32728173e
nataliehamel <Nataliestoker5@gmail.com>
2023-06-15 14:59:30 -0700

    days display

--- chamber/scripts/visit.js
+++ chamber/scripts/visit.js
@@ -1,30 +1,35 @@
-function getDaysSinceLastVisit() {
-    const visitTimestamp = localStorage.getItem("lastVisitTimestamp");
-    if (visitTimestamp) {
-      const lastVisit = new Date(parseInt(visitTimestamp));
-      const currentDate = new Date();
-      const timeDifference = currentDate.getTime() - lastVisit.getTime();
-      const daysSinceLastVisit = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-      return daysSinceLastVisit;
+// Function to calculate the number of days between two dates
+function calculateDaysSinceLastVisit() {
+    var lastVisit = localStorage.getItem("lastVisit");
+    var currentDate = new Date();
+    var timeDiff = Math.abs(currentDate - new Date(lastVisit));
+    var daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+    return daysDiff;
+}
+  
+// Function to update the visit information on the page
+function updateVisitInfo() {
+    var visitInfoElement = document.getElementById("visitInfo");
+    var daysSinceLastVisit = calculateDaysSinceLastVisit();
+    visitInfoElement.textContent = "Days since last visit: " + daysSinceLastVisit;
+}
+  
+// Function to set the last visit timestamp in localStorage
+function setLastVisitTimestamp() {
+    var currentDate = new Date();
+    localStorage.setItem("lastVisit", currentDate);
+}
+  
+// Check if the browser supports localStorage
+if (typeof Storage !== "undefined") {
+    // Check if the last visit timestamp is stored in localStorage
+    if (localStorage.getItem("lastVisit")) {
+      // Update visit information
+      updateVisitInfo();
+    } else {
+      // Set the last visit timestamp if it doesn't exist
+      setLastVisitTimestamp();
     }
-    return null;
+} else {
+    console.log("Sorry, your browser does not support Web Storage.");
 }
-  
-function displayVisitInfo() {
-    const daysSinceLastVisit = getDaysSinceLastVisit();
-    if (daysSinceLastVisit !== null) {
-      const visitInfo = document.getElementById("visitInfo");
-      visitInfo.textContent = `Days since your last visit: ${daysSinceLastVisit}`;
-    }
-}
-  
-function saveVisitTimestamp() {
-    const currentTimestamp = new Date().getTime();
-    localStorage.setItem("lastVisitTimestamp", currentTimestamp);
-}
-  
-// Call the necessary functions when the page loads
-window.addEventListener("load", () => {
-    displayVisitInfo();
-    saveVisitTimestamp();
-});
